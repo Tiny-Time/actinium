668922537639ca393aaa98eb109fa218e87e1b97
fawaziwalewa <wizardalani@gmail.com>
2023-06-02 08:56:00 +0100

    fix: update continue with email link

--- resources/views/auth/social-login.blade.php
+++ resources/views/auth/social-login.blade.php
@@ -60,7 +60,7 @@
             <span class="px-2 text-gray-500">OR</span>
             <hr class="flex-grow border-gray-500">
         </div>
-        <a href="{{ route('social-login') }}"
+        <a href="{{ route('login') }}"
             class="flex items-center justify-center w-full gap-2 py-2 mt-1 text-sm font-semibold text-white bg-red-400 rounded-lg">
             <svg class="w-5 h-5" viewBox="0 0 29 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M28.6724 3.0678L27.5467 1.94214L17.1196 12.3692C15.8162 13.6726 13.6242 13.6726 12.3208 12.3692L1.89372 2.00139L0.768066 3.12703L8.52913 10.8881L0.768066 18.6492L1.89372 19.7748L9.65478 12.0138L11.1951 13.5541C12.1431 14.502 13.3872 15.0352 14.6906 15.0352C15.994 15.0352 17.2381 14.502 18.186 13.5541L19.7264 12.0138L27.4875 19.7748L28.6131 18.6492L20.852 10.8881L28.6724 3.0678Z" fill="white"/>
