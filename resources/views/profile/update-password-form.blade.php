76acbaa2af02565d679a54af8a24f3095df50ad9
fawaziwalewa <wizardalani@gmail.com>
2023-06-18 19:03:04 +0100

    refactor: bg color for input on profile

--- resources/views/profile/update-password-form.blade.php
+++ resources/views/profile/update-password-form.blade.php
@@ -11,21 +11,21 @@
         <div
             class="px-4 py-5 bg-cyan-500 dark:bg-gray-800 sm:p-6 shadow {{ isset($actions) ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-t-md' }}">
             <div class="">
-                <div class="rounded-lg border-[1.7px] bg-white relative mt-4 w-full focus-within:border-indigo-500">
+                <div class="rounded-lg border-[1.7px] bg-cyan-500 relative mt-4 w-full focus-within:border-indigo-500">
                     <x-label class="rounded-b bg-cyan-500" for="current_password" value="{{ __('Current Password') }}" />
                     <x-input id="current_password" type="password" wire:model.defer="state.current_password"
                         autocomplete="current-password" />
                 </div>
                 <x-input-error for="current_password" class="mt-2" />
 
-                <div class="rounded-lg border-[1.7px] bg-white relative mt-4 w-full focus-within:border-indigo-500">
+                <div class="rounded-lg border-[1.7px] bg-cyan-500 relative mt-4 w-full focus-within:border-indigo-500">
                     <x-label class="rounded-b bg-cyan-500" for="password" value="{{ __('New Password') }}" />
                     <x-input id="password" type="password" wire:model.defer="state.password"
                         autocomplete="new-password" />
                 </div>
                 <x-input-error for="password" class="mt-2" />
 
-                <div class="rounded-lg border-[1.7px] bg-white relative mt-4 w-full focus-within:border-indigo-500">
+                <div class="rounded-lg border-[1.7px] bg-cyan-500 relative mt-4 w-full focus-within:border-indigo-500">
                     <x-label class="rounded-b bg-cyan-500" for="password_confirmation"
                         value="{{ __('Confirm Password') }}" />
                     <x-input id="password_confirmation" type="password" wire:model.defer="state.password_confirmation"
