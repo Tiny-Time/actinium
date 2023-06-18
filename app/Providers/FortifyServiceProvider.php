63116ad480b4200bf0ee3b6eb79a27952ec1051b
fawaziwalewa <wizardalani@gmail.com>
2023-06-18 21:52:19 +0100

    feat: add response page

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -48,7 +48,7 @@ class FortifyServiceProvider extends ServiceProvider
 
         RateLimiter::for('verification', function (Request $request) {
             $email = (string) $request->email;
-            return Limit::perMinute(1)->by($email.$request->ip());
+            return Limit::perMinute(5)->by($email.$request->ip());
         });
 
         /* ------------------------ Validate Google Recaptcha ----------------------- */
