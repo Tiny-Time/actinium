68611a4f5f63792113366d46cbad1384b6854139
fawaziwalewa <wizardalani@gmail.com>
2023-08-17 07:40:51 +0100

    chore: sort navigation

--- app/Filament/User/Pages/Subscription.php
+++ app/Filament/User/Pages/Subscription.php
@@ -8,5 +8,7 @@ class Subscription extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-credit-card';
 
+    protected static ?int $navigationSort = 4;
+
     protected static string $view = 'filament.user.pages.subscription';
 }
