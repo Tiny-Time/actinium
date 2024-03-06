24351cf6a2a005a4fc426336082c891ee1142be5
fawaziwalewa <fawaziwalewa@gmail.com>
2024-03-06 09:20:34 +0100

    fix: tag bug

--- resources/views/vendor/zeus/themes/zeus/sky/category.blade.php
+++ resources/views/vendor/zeus/themes/zeus/sky/category.blade.php
@@ -1,6 +1,6 @@
 <div>
     @unless ($posts->isEmpty())
-        <div class="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div class="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 px-4 sm:px-6 lg:px-8">
             @foreach ($posts as $post)
                 @include($skyTheme . '.partial.sticky')
             @endforeach
