223096a150178ec296377a26873ba35b88c9eaaa
fawaziwalewa <fawaziwalewa@gmail.com>
2024-04-05 09:24:57 +0100

    fix: file upload

--- resources/views/vendor/zeus/themes/zeus/sky/partial/sidebar/sponsored-post.blade.php
+++ resources/views/vendor/zeus/themes/zeus/sky/partial/sidebar/sponsored-post.blade.php
@@ -3,10 +3,10 @@
 @endphp
 @if ($imgSrc)
     <div>
-        <div class="text-xl font-bold flex gap-2 items-center">
+        <div class="flex items-center gap-2 text-xl font-bold">
             <h3>Sponsored</h3>
             <div class="h-[2px] grow bg-black"></div>
         </div>
-        <img src="{{ '/uploads/'.$imgSrc->image }}" alt="Sponsored Post" class="w-full mt-3">
+        <img src="{{ '/storage/'.$imgSrc->image }}" alt="Sponsored Post" class="w-full mt-3">
     </div>
 @endif
