3399e7d7851a45b0b95be0576dc533c0c43e4d22
fawaziwalewa <fawaziwalewa@gmail.com>
2024-04-06 09:00:38 +0100

    fix: image bug on frontend

--- packages/custom-lara-zeus/sky/src/Models/Page.php
+++ packages/custom-lara-zeus/sky/src/Models/Page.php
@@ -107,7 +107,7 @@ class Page extends Model implements HasMedia
         return $this->belongsTo(self::class, 'parent_id', 'id');
     }
 
-    public function image(string $collection = 'posts'): Collection | string | null
+    public function image(string $collection = 'pages'): Collection | string | null
     {
         if (! $this->getMedia($collection)->isEmpty()) {
             return $this->getFirstMediaUrl($collection);
