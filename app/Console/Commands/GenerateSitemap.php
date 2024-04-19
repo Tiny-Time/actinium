4da029e51f350235f87ce4ef4cfd6f145092da6f
fawaziwalewa <fawaziwalewa@gmail.com>
2024-04-19 10:20:13 +0100

    feat: auto sitemap generator

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Sitemap\SitemapGenerator;
+
+class GenerateSitemap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:generate-sitemap';
+
+    /**
+     * The console Generate sitemap.
+     *
+     * @var string
+     */
+    protected $description = 'Generate sitemap';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        SitemapGenerator::create(env('APP_URL'))->writeToFile('public/sitemap.xml');
+
+        $this->info('Sitemap generated successfully!');
+    }
+}
