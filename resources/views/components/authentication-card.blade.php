c7fd0a1044934d65d6e232009e29814a7b1801c2
fawaziwalewa <fawaziwalewa@gmail.com>
2024-04-21 09:11:53 +0100

    refactor: update fonts

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -12,25 +12,25 @@
         </div>
 
         <div class="font-medium w-full flex flex-col gap-3 text-sm">
-            <p>🌟 Unlock Over 800+ Templates - Dive into a vast collection of design options for every occasion!</p>
+            <p><span class="font-extrabold text-[#F87171]">🌟 Unlock Over 800+ Templates</span> - Dive into a vast collection of design options for every occasion! (Coming Soon) </p>
 
-            <p>📅 Auto-Save Events - Your events are automatically saved to your profile for quick and easy access.</p>
+            <p><span class="font-extrabold text-[#F87171]">📅 Auto-Save Events</span> - Your events are automatically saved to your profile for quick and easy access. (Coming Soon) </p>
 
-            <p>👀 RSVP Monitoring - See who's coming to your events with real-time RSVP updates.</p>
+            <p><span class="font-extrabold text-[#F87171]">👀 RSVP Monitoring</span> - See who's coming to your events with real-time RSVP updates.</p>
 
-            <p>💬 Manage Guest Interactions - View and delete guest comments and feedback seamlessly.</p>
+            <p><span class="font-extrabold text-[#F87171]">💬 Manage Guest Interactions</span> - View and delete guest comments and feedback seamlessly.</p>
 
-            <p>🔗 Custom Short URLs - Create personalized short URLs for your events to make sharing simpler.</p>
+            <p><span class="font-extrabold text-[#F87171]">🔗 Custom Short URLs</span> - Create personalized short URLs for your events to make sharing simpler. (Coming Soon) </p>
 
-            <p>📊 Advanced Tracking - Monitor the performance of your custom URLs with comprehensive tracking
+            <p><span class="font-extrabold text-[#F87171]">📊 Advanced Tracking</span> - Monitor the performance of your custom URLs with comprehensive tracking
                 capabilities.</p>
 
-            <p>📈 Detailed Event Reports - Get full insights on your events with reports on views, clicks, and sources
+            <p><span class="font-extrabold text-[#F87171]">📈 Detailed Event Reports</span> - Get full insights on your events with reports on views, clicks, and sources
                 analytics.</p>
 
-            <p>🎟️ RSVP to Events Effortlessly - Automatically save events you have RSVPed to for future reference.</p>
+            <p><span class="font-extrabold text-[#F87171]">🎟️ RSVP to Events Effortlessly</span> - Automatically save events you have RSVPed to for future reference. (Coming Soon) </p>
 
-            <p>🎉 All for FREE - Enjoy all these amazing features at no cost! Join us today and elevate your event
+            <p><span class="font-extrabold text-[#F87171]">🎉 All for FREE</span> - Enjoy all these amazing features at no cost! Join us today and elevate your event
                 planning!</p>
         </div>
     </div>
