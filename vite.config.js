140702c7f51f268681ce0cf40c70ab9760f3892c
fawaziwalewa <fawaziwalewa@gmail.com>
2024-07-02 18:33:15 +0100

    fix: resolve dynamic require issue with glob in Vite configuration using ES modules

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,25 @@
 import { defineConfig } from "vite";
 import laravel, { refreshPaths } from "laravel-vite-plugin";
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-const getThemeFiles = require("./getThemeFiles");
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                "resources/css/app.css",
-                "resources/css/custom.css",
-                "resources/js/app.js",
-                "resources/js/embed.js",
-                "resources/js/main.js",
-                "resources/js/clipboard.js",
-                ...getThemeFiles(),
-            ],
-            refresh: [...refreshPaths, "app/Http/Livewire/**"],
-        }),
-    ],
-});
+async function getConfig() {
+    const { default: getThemeFiles } = await import('./getThemeFiles.cjs');
+
+    return defineConfig({
+        plugins: [
+            laravel({
+                input: [
+                    "resources/css/app.css",
+                    "resources/css/custom.css",
+                    "resources/js/app.js",
+                    "resources/js/embed.js",
+                    "resources/js/main.js",
+                    "resources/js/clipboard.js",
+                    ...getThemeFiles(),
+                ],
+                refresh: [...refreshPaths, "app/Http/Livewire/**"],
+            }),
+        ],
+    });
+}
+
+export default getConfig();
