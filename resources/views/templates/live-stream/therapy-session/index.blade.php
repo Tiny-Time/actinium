9fbe71c12346553150410a9ca5769cc3b40e7dd7
fawaziwalewa <wizardalani267@gmail.com>
2024-10-19 06:55:19 +0100

    feat: add new templates

---
+++ resources/views/templates/live-stream/therapy-session/index.blade.php
@@ -0,0 +1,59 @@
+<x-template :event="$event" :userIP="$userIP">
+    <x-slot:css>
+        <link rel="stylesheet"
+            href="{{ Vite::asset('resources/views/templates/live-stream/therapy-session/css/style.css') }}" />
+        </x-slot>
+
+        <div class="toz-timer">
+            <!-- Days -->
+
+            <div class="toz-ec-d">
+                <img src="{{ Vite::asset('resources/views/templates/live-stream/therapy-session/images/timer_icon.webp') }}"
+                    alt="Timer icon">
+                <div>
+                    <div class="toz-days" id="toz-days">
+                        365
+                    </div>
+                    <span class="toz-unit">days</span>
+                </div>
+            </div>
+
+            <!-- Hours -->
+            <div class="toz-ec-d">
+                <img src="{{ Vite::asset('resources/views/templates/live-stream/therapy-session/images/timer_icon.webp') }}"
+                    alt="Timer icon">
+                <div>
+                    <div class="toz-hours" id="toz-hours">
+                        24
+                    </div>
+                    <span class="toz-unit">hours</span>
+                </div>
+            </div>
+
+            <!-- Minutes -->
+
+            <div class="toz-ec-d">
+                <img src="{{ Vite::asset('resources/views/templates/live-stream/therapy-session/images/timer_icon.webp') }}"
+                    alt="Timer icon">
+                <div>
+                    <div class="toz-mins" id="toz-mins">
+                        60
+                    </div>
+                    <span class="toz-unit">minutes</span>
+                </div>
+            </div>
+
+            <!-- Seconds -->
+
+            <div class="toz-ec-d">
+                <img src="{{ Vite::asset('resources/views/templates/live-stream/therapy-session/images/timer_icon.webp') }}"
+                    alt="Timer icon">
+                <div>
+                    <div class="toz-secs" id="toz-secs">
+                        60
+                    </div>
+                    <span class="toz-unit">seconds</span>
+                </div>
+            </div>
+        </div>
+</x-template>
